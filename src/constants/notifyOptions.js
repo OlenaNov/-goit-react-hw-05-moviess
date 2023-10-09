61f8f2227a4412b9bc63038d9d6e0dd72aff65ef
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-10-09 17:11:52 +0200

    fix

--- src/constants/notifyOptions.js
+++ src/constants/notifyOptions.js
@@ -4,10 +4,10 @@ export const notifyOptionsFailure = () => {
         timeout: 4000,
         width: '400px',
         svgSize: '80px',
-        backgroundColor: '${p => p.theme.colors.notifyBackground}',
+        backgroundColor: 'rgb(204, 0, 0, 0.7)',
         position: 'right-top',
         distance: '60px',
-        textColor: '${p => p.theme.colors.textStatic}',
+        textColor: 'rgb(255, 245, 238, 0.8)',
         fontSize: '20px',
       };
 };
